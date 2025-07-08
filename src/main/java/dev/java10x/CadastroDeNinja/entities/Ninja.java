c516a782ed3371122a48b4ffffc846f2d4c8d719
LucasELopes <lucaslopeslll412@gmail.com>
2025-07-08 16:45:00 -0300

    Cria uma entidade no banco de dados apartir do NinjaMOdel [Aula3]

---
+++ src/main/java/dev/java10x/CadastroDeNinja/entities/Ninja.java
@@ -0,0 +1,53 @@
+package dev.java10x.CadastroDeNinja.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_Cadastro")
+public class Ninja {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long $id;
+  private String nome;
+  private String email;
+  private int idade;
+
+  public Ninja(){
+  }
+
+  public Ninja(String nome, String email, int idade) {
+    this.nome = nome;
+    this.email = email;
+    this.idade = idade;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public int getIdade() {
+    return idade;
+  }
+
+  public void setIdade(int idade) {
+    this.idade = idade;
+  }
+
+}
